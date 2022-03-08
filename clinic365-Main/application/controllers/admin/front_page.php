a180067b07c24757dd3a7b362d244c3a768ce67f
Vibha Rao H R <vibharao08@gmail.com>
2022-03-08 11:31:37 +0530

    first commit

---
+++ clinic365-Main/application/controllers/admin/front_page.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+    <style media="screen">
+      h1,h2{
+        text-align: center;
+        color:black;
+        font-family:"poppins";
+      }
+      .center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  body{
+    background: #f2f2f2;
+  }
+  input{
+    text-align: center;
+    border-radius: 20px;
+    border-style: solid;
+    display: block ;
+    margin-left: auto;
+    margin-right: auto;
+    height:30px;
+    width:100px;
+    background-color:#f2f2f2;
+    cursor: pointer;
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+    transition-duration: 0.4s;
+    background-color:gray;
+    color: white; 
+  }
+  img{
+    width: 200px;
+  }
+}
+
+  
+  }
+    </style>
+  </head>
+  <body>
+    <h1>UNIVERSITY VISVESVARAYA COLLEGE OF ENGINEERING</h1>
+    <img src="Bangalore_University_logo.png" alt="" class="center" >
+<h2>Project Name:Courier Management System</h2>
+<h2>Submitted by:</h2>
+<h2>Vibha Rao H R  (19GANSE055)</h2> 
+<form class="" action="index.php" method="post">
+<input type="submit" name="" value="NEXT">
+</form>
+  </body>
+</html>
